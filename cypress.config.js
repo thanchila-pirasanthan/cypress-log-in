927981ecba7e33207b524053311de129dcd0e4a4
thanchila-pirasanthan <tsomanathan@gmail.com>
2022-06-10 15:03:21 +0100

    added cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,9 @@
-const { defineConfig } = require("cypress");
-
+const { defineConfig } = require('cypress');
+const cucumber = require('cypress-cucumber-preprocessor').default;
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('file:preprocessor', cucumber());
     },
   },
 });
